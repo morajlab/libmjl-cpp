97013cf95c4131703e1caa2a0aefa2862f2b2b95
morteza-jamali <mortezajamali4241@gmail.com>
2022-11-18 07:37:33 +0000

    feat(src): create `create_chain` source files

---
+++ src/create_chain/create_chain.c
@@ -0,0 +1,3 @@
+#include "create_chain.h"
+
+// TODO: Implement dynamic types in C
---
+++ src/create_chain/create_chain.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#define prepare_chain(T) \
+              struct Chain {\
+                  T value;\
+                  struct Chain *next;\
+              };\
+              static struct Chain *HEAD = NULL;\
+              struct Chain *insert_chain(T value) {\
+                  struct Chain *ptr = (struct Chain *) malloc(sizeof(struct Chain));\
+                  ptr->value = value;\
+                  ptr->next = HEAD;\
+                  HEAD = ptr;\
+                  return HEAD;\
+              }\
